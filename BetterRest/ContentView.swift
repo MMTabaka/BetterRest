c5eb23870c2fc688c6d39468c7b80794af59a4d2
MMTabaka <miloszmtabaka@gmail.com>
2022-06-24 15:16:11 +0100

    Working with dates

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,16 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
     
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
-            .labelsHidden()
-    }
-    
-    func exampleDate() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+        Text(Date.now.formatted(date: .long, time: .standard))
     }
 }
 
